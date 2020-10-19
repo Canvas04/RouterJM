e4db3e21465bfaf43788b1af4fe8b4f73709843c
Ruslan04071996 <ruslan040796@mail.ru>
2020-10-19 11:54:38 +0300

    Первый коммит

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-import 'normalize.css';
+import React from 'react'
+import { render } from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import { createLogger } from 'redux-logger'
+import thunk from 'redux-thunk'
+import rootReducer from './reducers'
+import App from './containers/App'
+
+const store = createStore(rootReducer)
+
+render(
+  <Provider store={store}>
+    <App />
+   </Provider> 
+  ,
+  document.getElementById('root')
+)

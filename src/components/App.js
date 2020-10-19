f19065a409c976abd4b9fd7e9a916ea3fa4e7b9d
Ruslan04071996 <ruslan040796@mail.ru>
2020-10-19 18:20:55 +0300

    Работают чекбоксы

--- src/components/App.js
+++ src/components/App.js
@@ -5,49 +5,50 @@ import { connect } from "react-redux";
 import stateCheck from "../reducers/index";
 
 const App = ({ stateCheck ,pressAll}) => {
-    
+   
+
+    const value =stateCheck
+    .filter((el) => el.id === 1)
+    .map((el) => el.checked)[0]
+     console.log(stateCheck,value);
   return (
     <div style={{ display: "flex", flexDirection: "column" }}>
-      <Checkbox onChange={pressAll} checked={stateCheck
+      <Checkbox onChange={pressAll} Checked={stateCheck
           .filter((el) => el.id === 1)
-          .map((el) => el.checked)
-          .join(" ")}>
+          .map((el) => el.checked)[0]
+          }>
         {stateCheck
           .filter((el) => el.id === 1)
           .map((el) => el.label)
           .join(" ")}
       </Checkbox>
-      <Checkbox checked={stateCheck
+      <Checkbox  onChange={() => {}} checked={stateCheck
           .filter((el) => el.id === 2)
-          .map((el) => el.checked)
-          .join(" ")}>
+          .map((el) => el.checked)[0]}>
         {stateCheck
           .filter((el) => el.id === 2)
           .map((el) => el.label)
           .join(" ")}
       </Checkbox>
-      <Checkbox checked={stateCheck
+      <Checkbox onChange={ () => {}} checked={stateCheck
           .filter((el) => el.id === 3)
-          .map((el) => el.checked)
-          .join(" ")}>
+          .map((el) => el.checked)[0]}>
         {stateCheck
           .filter((el) => el.id === 3)
           .map((el) => el.label)
           .join(" ")}
       </Checkbox>
-      <Checkbox checked={stateCheck
+      <Checkbox  onChange={() => {}} checked={stateCheck
           .filter((el) => el.id === 4)
-          .map((el) => el.checked)
-          .join(" ")}>
+          .map((el) => el.checked)[0]}>
         {stateCheck
           .filter((el) => el.id === 4)
           .map((el) => el.label)
           .join(" ")}
       </Checkbox>
-      <Checkbox checked={stateCheck
+      <Checkbox  onChange = {() => {}} checked={stateCheck
           .filter((el) => el.id === 5)
-          .map((el) => el.checked)
-          .join(" ")}>
+          .map((el) => el.checked)[0]}>
         {stateCheck
           .filter((el) => el.id === 5)
           .map((el) => el.label)

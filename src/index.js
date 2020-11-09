a44faf5473259e2474afeb1defd9c36923b7a2ea
Askarov Ruslan <askarow.rus@yandex.ru>
2020-11-09 12:54:19 +0300

    Улучшил линтер и prettier

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from "react";
-import { render } from "react-dom";
-import { Provider } from "react-redux";
-import { createStore } from "redux";
-import App from './components/App';
-import rootReducer from './reducers/index';
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import App from './components/App'
+import rootReducer from './reducers/index'
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer)
 render(
   <Provider store={store}>
     <App />
-   </Provider>
-  ,
-  document.getElementById("root")
-);
+  </Provider>,
+  document.getElementById('root')
+)
+

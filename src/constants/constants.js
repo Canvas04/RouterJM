88aa5d34339d32d8c8d86928c7b515f2571e1a85
Ruslan04071996 <askarow.rus@yandex.ru>
2020-12-19 17:05:50 +0300

    Add constants for url and articles

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,5 +1,9 @@
-export const ADD_TO_CART = 'ADD_TO_CART'
-export const CHECKOUT_REQUEST = 'CHECKOUT_REQUEST'
-export const CHECKOUT_SUCCESS = 'CHECKOUT_SUCCESS'
-export const CHECKOUT_FAILURE = 'CHECKOUT_FAILURE'
-export const RECEIVE_PRODUCTS = 'RECEIVE_PRODUCTS'
+export const articles = {
+  FETCH_REQUEST_ARTICLES: 'FETCH_REQUEST_ARTICLES',
+  FETCH_RECEIVE_ARTICLES: 'FETCH_RECEIVE_ARTICLES',
+  FETCH_FAILURE_ARTICLES: 'FETCH_FAILURE_ARTICLES',
+}
+
+export const baseUrl = {
+  url: 'https://conduit.productionready.io/api/',
+}

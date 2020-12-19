f82117318eafc95caa7ea754d78c76fea25316e4
Ruslan04071996 <askarow.rus@yandex.ru>
2020-12-19 14:49:31 +0300

    Change component on Articles

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
+import { Pagination } from 'antd'
 import React from 'react'
+import 'antd/dist/antd.css'
 import Registration from './registration/registration'
+import Articles from './articles/articles'
 
 export default function App() {
   return (
     <>
-      <Registration />
+      <Articles />
     </>
   )
 }

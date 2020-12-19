01658d4ab92bd68faa8c93bf60d9abea7a1e2b7c
Ruslan04071996 <askarow.rus@yandex.ru>
2020-12-19 15:38:42 +0300

    Add fonts about design

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { createStore } from 'redux'
 import App from './components/App'
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 import rootReducer from './redux/reducer'
+import './fonts/font.scss'
 
 const store = createStore(rootReducer)
 render(

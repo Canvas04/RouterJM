9a4d42dd77ca98e05e3c075133ed420b989e4490
Ruslan04071996 <askarow.rus@yandex.ru>
2020-12-19 15:59:08 +0300

    Add redux to devtoools

--- src/index.js
+++ src/index.js
@@ -1,18 +1,28 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import reduxThunk from 'redux-thunk'
 import App from './components/App'
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import rootReducer from './redux/reducer'
 import './fonts/font.scss'
 
-const store = createStore(rootReducer)
+const composeEnhancers =
+  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    : compose
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware( reduxThunk))
+)
+
 render(
   <Provider store={store}>
-      <Router >
-         <App />
-      </Router>
+    <Router>
+      <App />
+    </Router>
   </Provider>,
   document.getElementById('root')
 )

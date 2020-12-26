db6ef0968cd9c653916b90058638451a9cc47253
Ruslan04071996 <askarow.rus@yandex.ru>
2020-12-26 18:03:28 +0300

    Improve styles

--- src/components/artical-list/articles-list.js
+++ src/components/artical-list/articles-list.js
@@ -1,5 +1,35 @@
 import React from 'react'
 import styled from 'styled-components'
+import likeImage from './like.svg'
+import profileImg from './avatar.svg'
+
+export default () => {
+  const articlesItem = [1, 2, 3, 4, 5].map((el) => {
+    return (
+      <LiComponent key={el}>
+        <WrapperComponent>
+          <Header>Some article title</Header>
+          <LikeComponent>
+            <img height="14px" src={likeImage} />
+            <QuantityLikes>12</QuantityLikes>
+          </LikeComponent>
+          <ProfileImgWrapper>
+            <img src={profileImg} width="46px" height="46px" />
+          </ProfileImgWrapper>
+
+          <NameComponent>John Doe</NameComponent>
+          <DateComponent>March 5 , 2020</DateComponent>
+          <GenreArticle>Tag1</GenreArticle>
+        </WrapperComponent>
+      </LiComponent>
+    )
+  })
+  return (
+    <>
+      <UlComponent>{articlesItem}</UlComponent>
+    </>
+  )
+}
 
 const UlComponent = styled.ul`
   list-style: none;
@@ -12,14 +42,15 @@ const UlComponent = styled.ul`
 `
 const LiComponent = styled.li`
   padding: 15px 14px 24px 19px;
-  display: grid;
-  display:template-columns: 5fr 1fr;
-  display-columns-gap:85px;
-  background-color:#fff;
+  background-color: #fff;
   border-radius: 5px;
 `
-const TitleComponent = styled.div`
+const WrapperComponent = styled.div`
   display: grid;
+  grid-template-columns: 1fr 4fr 1fr 0.1fr;
+  grid-column-gap: 12px;
+  grid-template-rows: repeat(3, 22px);
+  grid-row-gap: 5px;
 `
 const Header = styled.h2`
   color: #1890ff;
@@ -27,24 +58,54 @@ const Header = styled.h2`
   font-size: 20px;
   line-height: 28px;
 `
-const Avatar = styled.div``
-const ArticleComponent = styled.div``
-export default () => {
-  const articlesItem = [1, 2, 3, 4, 5].map((el) => {
-    return (
-      <LiComponent key={el}>
-        <ArticleComponent>
-          <TitleComponent>
-            <Header>Some article title</Header>
-          </TitleComponent>
-        </ArticleComponent>
-        <Avatar></Avatar>
-      </LiComponent>
-    )
-  })
-  return (
-    <>
-      <UlComponent>{articlesItem}</UlComponent>
-    </>
-  )
-}
+const LikeComponent = styled.div`
+  position: relative;
+  top: 3px;
+`
+
+const QuantityLikes = styled.span`
+  margin-left: 5px;
+`
+
+const ProfileImgWrapper = styled.div`
+  grid-column: 4/5;
+  grid-row: 1/3;
+  display: flex;
+  justify-content: flex-end;
+`
+const NameComponent = styled.span`
+  grid-column: 3/4;
+  grid-row: 1/2;
+  display: flex;
+  justify-content: flex-end;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 18px;
+  line-height: 28px;
+  color: rgba(0, 0, 0, 0.85);
+`
+const DateComponent = styled.span`
+  grid-column: 3/4;
+  grid-row: 2/3;
+  display: flex;
+  justify-content: flex-end;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 12px;
+  line-height: 22px;
+  display: flex;
+  align-items: center;
+  color: rgba(0, 0, 0, 0.5);
+  position: relative;
+  top: -8px;
+`
+const GenreArticle = styled.div`
+  font-style: normal;
+  font-weight: normal;
+  font-size: 12px;
+  line-height: 15px;
+  color: rgba(0, 0, 0, 0.5);
+  border: 1px solid rgba(0, 0, 0, 0.5);
+  border-radius: 2px;
+`
+// Надо поместить жанры еще один контейнер , чтобы уменьшить границу

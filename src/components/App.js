0dbcf942b14c41b23f9dd833b4fd6a7581710e08
Ruslan04071996 <askarow.rus@yandex.ru>
2021-01-02 15:01:31 +0300

    Add useEffect

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,17 @@
 import { Pagination } from 'antd'
-import React from 'react'
+import React, { useEffect } from 'react'
 import 'antd/dist/antd.css'
 import Registration from './registration/registration'
 import Articles from './articles/articles'
+import { loadArticles } from '../redux/req-articles/action'
+import { useDispatch } from 'react-redux'
 
 export default function App() {
+  const dispatch = useDispatch()
+  useEffect(() => {
+      dispatch(loadArticles())
+      return () => dispatch(loadArticles())
+    })
   return (
     <>
       <Articles />

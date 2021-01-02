9de2221dc93816c381a261ecb1927d4199b8182d
Ruslan04071996 <askarow.rus@yandex.ru>
2021-01-02 16:12:02 +0300

    Add ErrorBoundary Component

---
+++ src/components/error-boundary/error-boundary.js
@@ -0,0 +1,46 @@
+import { render } from '@testing-library/react'
+import React, { Component } from 'react'
+import profile from '../artical-list/avatar.svg'
+import { Alert } from 'antd'
+import styled  from 'styled-components'
+class ErrorBoundary extends Component {
+  state = {
+    hasError: false,
+  }
+  static getDerivedStateFromError() {
+    return { hasError: true }
+  }
+
+  render() {
+    const { type, isErrorDurationLoading } = this.props
+    const { hasError } = this.state
+    if (type === 'img') {
+      if (hasError) {
+        return <img src={profile} alt="profile" />
+      }
+      return this.props.children
+    }
+    if (type === 'common') {
+      if (isErrorDurationLoading) {
+        return (
+          <>
+            <WrapperForError>
+              <Alert message={'Error'} 
+              type={'error'}
+              description={'The server isn\'t working temporarily.Please,try to reload the page!'}
+              />
+            </WrapperForError>
+          </>
+        )
+      }
+      return this.props.children
+    }
+  }
+}
+export default ErrorBoundary
+
+const WrapperForError = styled.div`
+  display: flex;
+  margin-top:20px;
+  justify-content: center;
+`

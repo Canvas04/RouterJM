aef24010fd3b7d7d6fbf031d8e4cb697a8dc21e2
Ruslan04071996 <askarow.rus@yandex.ru>
2021-01-05 13:31:51 +0300

    Add comp Switch to App component

--- src/components/App.js
+++ src/components/App.js
@@ -5,16 +5,28 @@ import Registration from './registration/registration'
 import Articles from './articles/articles'
 import { loadArticles } from '../redux/req-articles/action'
 import { useDispatch } from 'react-redux'
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import FullArticle from './full-article/full-artilce'
 export default function App() {
   const dispatch = useDispatch()
   useEffect(() => {
-      dispatch(loadArticles())
-      return () => dispatch(loadArticles())
-    })
+    dispatch(loadArticles())
+    return () => dispatch(loadArticles())
+  })
   return (
     <>
-      <Articles />
+      <Switch>
+        <Route path="/" exact component={Articles}></Route>
+        <Route path="/articles" component={Articles} exact />
+        <Route
+          path="/articles/:slug"
+          render={({match}) => {
+            const {slug} = match.params
+            
+            return <FullArticle id={slug} />
+          }}
+        />
+      </Switch>
     </>
   )
 }

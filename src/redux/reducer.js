30c38aa5c4541e14dafe02fa188c59cf9d39a6df
Ruslan04071996 <askarow.rus@yandex.ru>
2021-01-09 15:51:10 +0300

    Add to rootReducer sign_in

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux'
 import loadArticles from './req-articles/req-articles'
 import sign_up from './sign-up/sign-up'
-export default combineReducers({ loadArticles,sign_up })
+import sign_in from './sign-in/sign-in'
+export default combineReducers({ loadArticles,sign_up ,sign_in})

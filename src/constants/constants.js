6986a60c5047ecec03c61bbca0cb56eee7b39f1b
Ruslan04071996 <askarow.rus@yandex.ru>
2021-01-09 13:57:10 +0300

    Add constant registration

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -6,4 +6,12 @@ export const articles = {
 
 export const baseUrl = {
   url: 'https://conduit.productionready.io/api/',
+  sign_up: 'users'
+}
+
+export const registration = {
+  SIGN_UP_REQUEST: 'SIGN_UP_REQUEST',
+  SIGN_UP_SUCCESS: 'SIGN_UP_SUCCESS',
+  SIGN_UP_WRONG: 'SIGN_UP_WRONG',
+  SIGN_IN: 'SIGN_IN',
 }

e53149460b8991e43d3146047bd55e8df713b61d
Ruslan04071996 <askarow.rus@yandex.ru>
2021-01-09 13:58:04 +0300

    Add reducer sign_up to root

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,4 +1,4 @@
 import { combineReducers } from 'redux'
 import loadArticles from './req-articles/req-articles'
-
-export default combineReducers({ loadArticles })
+import sign_up from './sign-up/sign-up'
+export default combineReducers({ loadArticles,sign_up })

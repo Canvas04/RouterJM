7e11cad16b96e5e0bf7a6389a3d736225d7849dc
Ruslan04071996 <askarow.rus@yandex.ru>
2021-01-13 17:09:08 +0300

    Add initialState

--- src/redux/sign-in/sign-in.js
+++ src/redux/sign-in/sign-in.js
@@ -1,15 +1,27 @@
 import { registration } from '../../constants/constants'
 
 const { SIGN_IN_REQUEST, SIGN_IN_SUCCESS, SIGN_IN_WRONG } = registration
+const initialState = {
+  isLogin: false,
+  user: {
+    id: '',
+    email: '',
+    createdAt: '',
+    username: '',
+    bio: null,
+    image: null,
+    token: ' ',
+  },
+}
 
-export default (state = '', action) => {
+export default (state = { isLogin: false }, action) => {
   switch (action.type) {
     case SIGN_IN_SUCCESS:
-      return action.payload
+      return { ...action.payload, isLogin: true }
 
     case SIGN_IN_WRONG:
       const invalidData = action.payload['email or password']
-      return { invalidData }
+      return { invalidData, isLogin: false }
     default:
       return state
   }

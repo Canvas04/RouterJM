08519e410654531aa1c7b998a4133db59e0763c8
Ruslan04071996 <askarow.rus@yandex.ru>
2021-01-13 17:07:59 +0300

    Add reducer userState to root

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -2,4 +2,5 @@ import { combineReducers } from 'redux'
 import loadArticles from './req-articles/req-articles'
 import sign_up from './sign-up/sign-up'
 import sign_in from './sign-in/sign-in'
-export default combineReducers({ loadArticles,sign_up ,sign_in})
+import userState from './userState/userState'
+export default combineReducers({ loadArticles,sign_up ,sign_in,userState})

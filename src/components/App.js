45e628bce415ea29cbcbfeae8ab0fd4bd157d17c
Ruslan04071996 <askarow.rus@yandex.ru>
2021-01-13 16:59:26 +0300

    Add saving user in useEffect

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,36 @@
 import { Pagination } from 'antd'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import 'antd/dist/antd.css'
 import Registration from './registration/registration'
 import Articles from './articles/articles'
 import { loadArticles } from '../redux/req-articles/action'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import FullArticle from './full-article/full-artilce'
 import SignUp from './registration-page/registration-page'
 import SignIn from './sign-in/sign-in'
+import ChangingProfile from './change-profile/change-profile'
+import login from '../redux/userState/login-action'
 
 export default function App() {
+
   const dispatch = useDispatch()
+  const savedEmail = getCookie('email')
+  const savedPassword = getCookie('password')
+  const objForRegistration = {
+    user: {
+      email: savedEmail,
+      password:savedPassword
+    },
+  }
   useEffect(() => {
     dispatch(loadArticles())
+    dispatch(login(JSON.stringify(objForRegistration), 'users', 'login'))
     return () => dispatch(loadArticles())
   })
+
+
+
   return (
     <>
       <Switch>
@@ -29,9 +44,19 @@ export default function App() {
             return <FullArticle id={slug} />
           }}
         />
+        
         <Route path="/sign-up" exact component={SignUp} />
         <Route path="/sign-in" component={SignIn} />
+        <Route path="/profile" component={ChangingProfile} />
       </Switch>
     </>
   )
+}
+
+
+function getCookie(name) {
+  let matches = document.cookie.match(new RegExp(
+    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+  ));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
 }

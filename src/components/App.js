6e7355f60a62b5596b9ad4db243e15e8e0ce6df9
Ruslan04071996 <askarow.rus@yandex.ru>
2021-01-16 14:26:40 +0300

    Remove useless vars

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
-import { Pagination } from 'antd'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import 'antd/dist/antd.css'
 import Registration from './registration/registration'
 import Articles from './articles/articles'
 import { loadArticles } from '../redux/req-articles/action'
 import { useDispatch, useSelector } from 'react-redux'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom'
 import FullArticle from './full-article/full-artilce'
 import SignUp from './registration-page/registration-page'
 import SignIn from './sign-in/sign-in'
@@ -14,6 +17,7 @@ import login from '../redux/userState/login-action'
 import NewArticle from './new-article/new-article'
 
 export default function App() {
+  const isLogin = useSelector((store) => store.userState.isLogin)
 
   const dispatch = useDispatch()
   const savedEmail = getCookie('email')
@@ -21,7 +25,7 @@ export default function App() {
   const objForRegistration = {
     user: {
       email: savedEmail,
-      password:savedPassword
+      password: savedPassword,
     },
   }
   useEffect(() => {
@@ -30,35 +34,36 @@ export default function App() {
     return () => dispatch(loadArticles())
   })
 
-
-
   return (
     <>
       <Switch>
-        <Route path="/" exact component={Articles}></Route>
-        <Route path="/articles" component={Articles} exact />
+        <Route path='/' exact component={Articles}></Route>
+        <Route path='/articles' component={Articles} exact />
         <Route
-          path="/articles/:slug"
+          path='/articles/:slug'
           render={({ match }) => {
             const { slug } = match.params
 
             return <FullArticle id={slug} />
           }}
         />
-        
-        <Route path="/sign-up" exact component={SignUp} />
-        <Route path="/sign-in" component={SignIn} />
-        <Route path="/profile" component={ChangingProfile} />
+
+        <Route path='/sign-up' exact component={SignUp} />
+        <Route path='/sign-in' component={SignIn} />
+        <Route path='/profile' component={ChangingProfile} />
         <Route path='/new-article' component={NewArticle} />
       </Switch>
     </>
   )
 }
 
-
 function getCookie(name) {
-  let matches = document.cookie.match(new RegExp(
-    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
-  ));
-  return matches ? decodeURIComponent(matches[1]) : undefined;
+  let matches = document.cookie.match(
+    new RegExp(
+      '(?:^|; )' +
+        name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') +
+        '=([^;]*)'
+    )
+  )
+  return matches ? decodeURIComponent(matches[1]) : undefined
 }

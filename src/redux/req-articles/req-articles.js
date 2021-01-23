550259f5019d0f0d8795f55df9e8b0dd71a83793
Ruslan04071996 <askarow.rus@yandex.ru>
2021-01-23 18:19:33 +0300

    Add new case for reducer

--- src/redux/req-articles/req-articles.js
+++ src/redux/req-articles/req-articles.js
@@ -18,6 +18,26 @@ export default (state = initialState, action) => {
       return { ...state, isFetching: true }
     case FETCH_RECEIVE_ARTICLES:
       return { ...state, isFetching: false, articles: action.payload }
+    case 'ESTIMATE':
+      const { article: newArticle } = action.payload
+
+      const { slug } = newArticle
+      const prevArticles = state.articles.articles
+      const newArticles = prevArticles.map((el) => {
+        if (el.slug === slug) {
+          const { favoritesCount, favorited } = el
+          return {
+            ...el,
+            favorited: newArticle.favorited,
+            favoritesCount: newArticle.favorited
+              ? el.favoritesCount + 1
+              : el.favoritesCount - 1,
+          }
+        }
+        return { ...el }
+      })
+      const newObj = { ...state.articles, articles: newArticles }
+      return { ...state, articles: newObj }
     case FETCH_FAILURE_ARTICLES:
       return { ...state, isError: true }
     default:

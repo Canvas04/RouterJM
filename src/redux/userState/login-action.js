2d95ed8efd02678b94714c082074d23f0ad20394
Ruslan04071996 <askarow.rus@yandex.ru>
2021-01-23 18:19:10 +0300

    Add actions for estimating  Articles

--- src/redux/userState/login-action.js
+++ src/redux/userState/login-action.js
@@ -24,6 +24,8 @@ const wrongLogin = (payload) => ({
   payload,
 })
 
+const estimate = (payload) => ({type: 'ESTIMATE',payload})
+
 export default (user, url1 = '',url2='',token='',method='POST') => {
   return async (dispatch) => {
     
@@ -53,5 +55,32 @@ export default (user, url1 = '',url2='',token='',method='POST') => {
     }
   }
 }
+
+export const estimateArticle = (user, url1 = '',url2='',token='',method='POST') => {
+  return async (dispatch) => {
+    
+    try {
+      const attemptSignIn = await fetch(`${url}${url1}/${url2}`, {
+        method: method,
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8',
+           Authorization: token 
+        },
+       
+      })
+
+      if (attemptSignIn.ok) {
+        const successRequest = await attemptSignIn.json()
+        dispatch(estimate(successRequest))
+      } else {
+        const dataAboutErrors = await attemptSignIn.json()
+        throw dataAboutErrors
+      }
+    } catch (error) {
+      // dispatch(wrongLogin(error.errors))
+    }
+  }
+}
+
 
 export const logOut = () => ({ type: LOG_OUT })
